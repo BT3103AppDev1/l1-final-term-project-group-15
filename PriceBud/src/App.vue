3070c4b6c8837c0053e61d91b0d65b1a390ba4d8
ignatius <ignatius.choe@u.nus.edu>
2024-03-28 17:30:29 +0800

    second commit

--- PriceBud/src/App.vue
+++ PriceBud/src/App.vue
@@ -1,20 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import RowCards from './components/RowCards.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <div id="wishlist">
+    <RowCards />
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="insights">
+    <RowCards />
+  </div>
+
 </template>
 
 <style scoped>
--- PriceBud/src/components/RowCards.vue
+++ PriceBud/src/components/RowCards.vue
@@ -1,61 +1,52 @@
 <template>
-    <div class="product-card">
-        <!-- Your template code here -->
-        <h3 class="poppins-regular">{{ product.name }}</h3>
-        <img class="product-image" :src="product.image" alt="product image">
-        <div class="price-container">
-            <svg-icon 
-                class="icon"
-                type="mdi" 
-                :path="mdiArrowDown"
-                color="red">
-            </svg-icon> 
-            <h1 class="poppins-semibold">{{ product.price }}</h1>
-        </div>
+    <div class="content-container">
+        <v-sheet color="FFFFFF" elevation="4">
+            <h1 class="title">{{ title }}</h1>
+            <h3 class="desc">{{ description }}</h3>
+            
+            <!-- Product Cards -->
+           <product-card />
+
+        </v-sheet>
     </div>
+
 </template>
+
 
 <script setup>
 import { defineProps } from 'vue'
+import ProductCard from './ProductCard.vue'
 
-// import icons
-import SvgIcon from '@jamescoyle/vue-icon'
-import { mdiArrowDown } from '@mdi/js'
 
 defineProps({
-  product: {
-    type: Object,
-
-    // placeholder product info
-    default: () => ({
-      name: 'Apple iPhone 14',
-      image: `https://imagedelivery.net/JAV112JY973Crznn4xb8Sg/41816e80-a4b4-47bb-de71-cc19edbfb400/mobile`,
-      price: '$799',
-    }),
-  },
+    title: String,
+    description: String
 })
-// Other options here
 </script>
 
+
 <style scoped>
-/* Your component styles here */
-.product-card {
-    width: 25%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-.price-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+.title {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 700; /* Semi-Bold */
+    font-style: normal;
 }
 
-.product-image {
-    width: 70%;
+.desc {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400; /* Regular */
+    font-style: normal;
+    color: #7C8DB5;
+
 }
 
+.v-sheet {
+    padding: 2%;
+}
 
+.content-container {
+    width: 70vw;
+    height: 20vh;
+}
 
 </style>
