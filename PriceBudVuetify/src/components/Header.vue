332ddf245b499a0241696f394b7b6a148873a59a
Teckkaiii <158819355+Teckkaiii@users.noreply.github.com>
2024-03-29 10:02:19 +0800

    changes to Header

---
+++ PriceBudVuetify/src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-toolbar density="compact" class="custom-toolbar">
+      <v-card-title>Welcome Back, {{ username }}
+        <p>{{ content }}</p>
+      </v-card-title>
+      <v-spacer></v-spacer>
+      <v-icon icon="mdi-trophy" />
+
+      <v-card-subtitle>{{ username }} {{ Surname }}</v-card-subtitle>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+  </template>
+  
+  <script>
+
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Settings' },
+        { title: 'Edit Profile' },
+        { title: 'Log Out' },
+      ],
+      username: 'Marci',
+      content: 'Here is the information about the searched product',
+      Surname: 'Fumons'
+    })
+  }
+  </script>
+  
+  <style scoped>
+  .custom-toolbar {
+    font-family: 'poppins'; 
+    font-weight: bold;
+}
+  .custom-toolbar p {
+    font-family: 'poppins';
+    font-size: 11px; 
+    font-weight: bold;
+  }
+
+  .v-card-title {
+    font-weight: bold;
+  }
+
+
+  </style>
+  
