23bc64a47b955df16624f76860f7faeddcbbe266
bachng0111 <e0969877@u.nus.edu>
2024-04-03 15:27:35 +0800

    Update SideBar.vue

--- PriceBudVuetify/src/components/SideBar.vue
+++ PriceBudVuetify/src/components/SideBar.vue
@@ -1,80 +1,103 @@
 <template>
-    <v-app>
-      <v-navigation-drawer v-model="drawer" app>
-        <!-- Sidebar content -->
-        <v-list>
-          <v-list-item link to="/HomePage">
-            <v-list-item-icon>
-              <v-icon>mdi-view-dashboard</v-icon>
-            </v-list-item-icon>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <!-- Logo and Title -->
+      <div class="d-flex align-center mt-5">
+        <img src="/Users/bachnguyen/Downloads/Screenshot_2024-04-03_at_2.43.16_PM-removebg-preview.png" alt="Logo" class="mr-2 ml-4" style="height: 24px;"> <!-- Adjust height as needed -->
+        <span class="title"><strong>PriceBud</strong></span>
+      </div>
+      <!-- Sidebar content -->
+      <v-list>
+        <v-list-item link to="/HomePage">
+          <v-list-item-icon>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
             <v-list-item-title>Home Page</v-list-item-title>
-          </v-list-item>
-          <v-list-item link to="/Wishlist">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/Wishlist">
+          <v-list-item-icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
             <v-list-item-title>Wishlist</v-list-item-title>
-          </v-list-item>
-          <v-list-item link to="/Profile">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/Profile">
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
             <v-list-item-title>Profile</v-list-item-title>
-          </v-list-item>
-          <v-list-item link to="/Community">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/Community">
+          <v-list-item-icon>
+            <v-icon>mdi-account-group</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
             <v-list-item-title>Community</v-list-item-title>
-          </v-list-item>
-          <v-list-item link to="/Setting">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Setting</v-list-item-title>
-          </v-list-item>
-          <v-list-item link to="/Help Centre">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/Setting">
+          <v-list-item-icon>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-list class = "bottom-sidebar-items">
+        <v-list-item link to="/Help Centre">
+          <v-list-item-icon>
+            <v-icon>mdi-help-circle</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
             <v-list-item-title>Help Centre</v-list-item-title>
-          </v-list-item>
-          <v-list-item link to="/Contact us">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/Contact us">
+          <v-list-item-icon>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
             <v-list-item-title>Contact us</v-list-item-title>
-          </v-list-item>
-          <v-list-item link to="/Log out">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/Log out">
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
             <v-list-item-title>Log out</v-list-item-title>
-          </v-list-item>
-          <!-- Add more sidebar items as needed -->
-        </v-list>
-      </v-navigation-drawer>
-  
-      <!-- Main content area -->
-      <v-app-bar app>
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>My App</v-toolbar-title>
-      </v-app-bar>
-  
-      <v-main>
-        <!-- Page content goes here -->
-        <router-view></router-view>
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        drawer: false
-      };
-    }
-  };
-  </script>
-  
+          </v-list-item-content>
+        </v-list-item>
+        <!-- Add more sidebar items as needed -->
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Main content area -->
+    <v-main>
+      <!-- Page content goes here -->
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true
+    };
+  }
+};
+</script>
+ <style scoped>
+ .bottom-sidebar-items {
+  margin-top: 150%;
+ }
+</style>
