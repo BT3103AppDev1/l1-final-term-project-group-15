3697d539995542c07dde5f1fa0721e10d0cb2743
ignatius <ignatius.choe@u.nus.edu>
2024-04-15 02:57:57 +0800

    added logout functionality at SideBar

--- PriceBudVuetify/src/components/SideBar.vue
+++ PriceBudVuetify/src/components/SideBar.vue
@@ -16,12 +16,20 @@
             <v-list-item-title>Home Page</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link to="/WishListPage">
+        <v-list-item link to="/WishList">
           <v-list-item-icon>
             <v-icon>mdi-heart</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>Wishlist</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/ProductPage2">
+          <v-list-item-icon>
+            <v-icon>mdi-cart</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Product</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link to="/Profile">
@@ -67,7 +75,7 @@
             <v-list-item-title>Contact us</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item link to="/Log out">
+        <v-list-item @click="logout">
           <v-list-item-icon>
             <v-icon>mdi-logout</v-icon>
           </v-list-item-icon>
@@ -87,15 +95,21 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      drawer: true
-    };
-  }
-};
+<script setup>
+  import { ref } from 'vue';
+  import { useStore } from 'vuex';
+  import { useRouter } from 'vue-router';
+
+  const drawer = ref(true);
+  const store = useStore();
+  const router = useRouter();
+
+  const logout = () => {
+    store.commit('setLoggedIn', false);
+    router.push('/HomePage');
+  };
 </script>
+
 <style scoped>
  .bottom-sidebar-items {
   margin-top: 50%;
