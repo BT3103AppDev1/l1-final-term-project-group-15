9e62089c264ec9a3527bf983066a74b1d3e84512
ignatius <ignatius.choe@u.nus.edu>
2024-04-18 18:18:34 +0800

    router removed auto

--- PriceBudVuetify/src/router/index.js
+++ PriceBudVuetify/src/router/index.js
@@ -6,10 +6,50 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+// import { createRouter, createWebHistory } from 'vue-router'
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       redirect: '/HomePage'
+//     },
+//     // other routes...
+//   ]
+// })
+
+// export default router
+
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../pages/HomePage.vue'
+import IndexPage from '../pages/index.vue'
+// import LogIn1 from '../pages/Login1.vue'
+import LoginPage from '../pages/LoginPage.vue'
+import LogOut1Page from '../pages/LogOut1.vue'
+import ProductPage1 from '../pages/ProductPage1.vue'
+import ProductPage2 from '../pages/ProductPage2.vue'
+import ProfilePage from '../pages/ProfilePage.vue'
+import SearchResultsPage from '../pages/searchResults.vue'
+import SignupPage from '../pages/SignupPage.vue'
+import WishListPage from '../pages/WishListPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', redirect: '/HomePage' },
+    { path: '/HomePage', component: HomePage },
+    { path: '/index', component: IndexPage },
+    // { path: '/LogIn1', component: LogIn1 },
+    { path: '/LoginPage', component: LoginPage },
+    { path: '/LogOut1', component: LogOut1Page },
+    { path: '/ProductPage1', component: ProductPage1 },
+    { path: '/ProductPage2', component: ProductPage2 },
+    { path: '/ProfilePage', component: ProfilePage },
+    { path: '/searchResults', component: SearchResultsPage },
+    { path: '/SignupPage', component: SignupPage },
+    { path: '/WishListPage', component: WishListPage },
+  ]
 })
 
 export default router
