08dec1c7e137e96f1f6c370b8832bb1b77eaafc9
Teckkaiii <158819355+Teckkaiii@users.noreply.github.com>
2024-04-24 01:03:14 +0800

    adding a button to go back

--- PriceBudVuetify/src/components/AddButton.vue
+++ PriceBudVuetify/src/components/AddButton.vue
@@ -1,47 +1,59 @@
 <template>
-    <v-container class = "container">
-        <v-btn class="add-button" color="blue" small rounded @click="addProduct"> 
-            <v-icon>mdi-plus</v-icon>
-        </v-btn>
+    <v-container class="container">
+      <v-btn class="left-button" color="blue" @click="goBack"> 
+        <v-icon>mdi-arrow-left</v-icon>
+    </v-btn>
+      <v-btn class="right-button" color="blue" @click="addProduct"> 
+        <v-icon>mdi-plus</v-icon>
+    </v-btn>
     </v-container>
-</template>
-
-<script>
-import { getFirestore, doc, updateDoc, arrayUnion } from "firebase/firestore";
-export default {
+  </template>
+  
+  <script>
+  import { getFirestore, doc, updateDoc, arrayUnion } from "firebase/firestore";
+  
+  export default {
     data() {
-        return {}
+      return {}
     },
             
     props: {
-        product: String,
-        userEmail: String
+      product: String,
+      userEmail: String
     },
-
+  
     methods: {
-        async addProduct() {
+      async addProduct() {
         const db = getFirestore();
         const userRef = doc(db, 'Users', this.userEmail);
-
+  
         // Atomically add a new product ID to the "wishlist" array field.
         await updateDoc(userRef, {
-            Wishlist: arrayUnion(this.product)
-         });
-
+          Wishlist: arrayUnion(this.product)
+        });
+  
         this.$router.push({ name: 'ProductToWishList', params: { id: this.product } });
-        }
+      },
+      goBack() {
+        this.$router.go(-1)
+      }
     }
-}
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    flex-direction: row ;
+  }
+  .right-button{
+    margin-left: auto;
+    margin-right: 10px;
+    border-radius: 35px;
+  }
+  .left-button{
+    border-radius: 35px;
 
-</script>
-
-<style>
-    .container {
-        display: flex;
-    }
-    .add-button {
-        width: 50px;
-        margin-left: auto;
-        justify-content: center;
-    }
-</style>
+  }
+  </style>
+  
