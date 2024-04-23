d05b96b3f39d7b3ae3445cdc68ea5afb6c84e042
bachng0111 <e0969877@u.nus.edu>
2024-04-23 18:51:39 +0800

    Update ProfileHeader.vue

--- PriceBudVuetify/src/components/ProfileHeader.vue
+++ PriceBudVuetify/src/components/ProfileHeader.vue
@@ -3,7 +3,7 @@
       <v-row align = "center">
         <v-col cols ="2" class = "pic">
           <v-avatar size="100">
-            <img class = "avatar" src="/Users/bachnguyen/Desktop/Screenshot 2024-04-23 at 12.20.36 AM.png" alt="Profile Picture">
+            <img class = "avatar" src="https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FShashank_%2528director%2529&psig=AOvVaw2WPJfL4rfytg02dl2Ft273&ust=1713955819255000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCIiXspCV2IUDFQAAAAAdAAAAABAI" alt="Profile Picture">
           </v-avatar>
         </v-col>
         <v-col cols = 3 class = "name">
