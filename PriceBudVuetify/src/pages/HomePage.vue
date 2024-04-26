a1583bb5b9e7f822283a5c811fcfedb76b868f05
seanljh <156348930+seanljh@users.noreply.github.com>
2024-04-26 16:13:19 +0800

    Revert "Merge branch 'main' of https://github.com/BT3103AppDev1/l1-final-term-project-group-15"
    This reverts commit 9d973939a4072f5aa3f41532ec316d1a2be60087, reversing
    changes made to 4a551c54d6973a136b0675577e2d9e4c10ff3fa3.

--- PriceBudVuetify/src/pages/HomePage.vue
+++ PriceBudVuetify/src/pages/HomePage.vue
@@ -11,9 +11,6 @@
       <MasterInput v-if="userEmail == 'master@gmail.com'" />
       <UserPriceInput />
       <RowCards :wishlist = "wishlist" :isLoggedIn="true" />
-
-      <!-- for testing if the top5ReviewedProductsActual is being fetched from the database -->
-      <!-- <h1>{{ top5ReviewedProductsActual }}</h1> -->
                 
     </v-container>
 
@@ -22,6 +19,7 @@
       <SearchBar />
       <HomeLoginComp />
       <RowCards :isLoggedIn="false"/>
+      
     </v-container>
 
   </v-sheet>
@@ -36,7 +34,7 @@ import HomeLoginComp from '../components/HomeLoginComp.vue'
 import MasterInput from '../components/MasterInput.vue'
 import UserPriceInput from '../components/UserPriceInput.vue'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import { getFirestore, collection, doc, getDoc, getDocs, setDoc } from 'firebase/firestore'
+import { getFirestore, collection, doc, getDoc, setDoc } from 'firebase/firestore'
 import { ref,  onBeforeMount } from 'vue'
 
 
@@ -58,56 +56,6 @@ onBeforeMount(() => {
     return user.metadata.creationTime === user.metadata.lastSignInTime;
   }
 
-  // async function getTop5ReviewedProducts() {
-  //   const db = getFirestore();
-  //   const querySnapshot = await getDocs(collection(db, 'Products'));
-  //   let top5ReviewedProducts = [];x
-
-  //   querySnapshot.forEach((doc) => {
-  //     const product = doc.data();
-  //     product.id = doc.id; // Include the document ID in the product data
-  //     const entry = {
-  //       id: product.id,
-  //       Ratings: product.Ratings
-  //     }
-  //     top5ReviewedProducts.push(entry);
-  //   });
-
-  //   top5ReviewedProducts = top5ReviewedProducts
-  //     .sort((a, b) => b.Ratings - a.Ratings)
-  //     .slice(0, 5);
-
-  //   console.log(top5ReviewedProducts)
-  //   top5ReviewedProductsActual = top5ReviewedProducts
-  //   console.log(top5ReviewedProductsActual)
-  // }
-
-  // async function getTop5WishlistedProducts() {
-  //   const db = getFirestore();
-  //   const querySnapshot = await getDocs(collection(db, 'Products'));
-  //   let top5WishlistedProducts = [];
-
-  //   querySnapshot.forEach((doc) => {
-  //     const product = doc.data();
-  //     product.id = doc.id; // Include the document ID in the product data
-  //     const entry = {
-  //       id: product.id,
-  //       NumWishlisted: product["Number Of Wishlists"]
-  //     }
-  //     top5WishlistedProducts.push(entry);
-  //   });
-
-  //   top5WishlistedProducts = top5WishlistedProducts
-  //     .sort((a, b) => b.NumWishlisted - a.NumWishlisted)
-  //     .slice(0, 5);
-
-  //   console.log(top5WishlistedProducts)
-  //   top5ReviewedProductsActual = top5WishlistedProducts
-  //   console.log(top5ReviewedProductsActual)
-  // }
-
-
-
   const newUserUpdate = onAuthStateChanged(auth, async (user) => {
     if (user) {
       isLoggedIn.value = true
@@ -115,8 +63,6 @@ onBeforeMount(() => {
 
       if (isFirstTimeLogin) {
         console.log("First time login!");
-        // getTop5ReviewedProducts()
-        // getTop5WishlistedProducts()
 
         const db = getFirestore()
         const userCollection = collection(db, 'Users')
@@ -146,31 +92,24 @@ onBeforeMount(() => {
         userEmail.value = user.email
         
         const db = getFirestore()
-        const docRefUser = doc(db, 'Users', user.email)
-        const docSnapUser = await getDoc(docRefUser)
+        const docRef = doc(db, 'Users', user.email)
+        const docSnap = await getDoc(docRef)
 
-        if (docSnapUser.exists()) {
-          const data = docSnapUser.data()
+        if (docSnap.exists()) {
+          const data = docSnap.data()
           console.log(data.Wishlist)
           wishlist.value = data.Wishlist
         }
         console.log(wishlist.value)
-        // getTop5ReviewedProducts()
-        // getTop5WishlistedProducts()
       }
     } else {
       isLoggedIn.value = false
-      // getTop5ReviewedProducts()
-      // getTop5WishlistedProducts()
     }
   
   })
 
   return newUserUpdate;
 })
-
-
-
 
 
 </script>
